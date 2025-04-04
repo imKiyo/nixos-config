86cf91064af7939f6570f3f1671bb72b907aaf38
imKiyo <xargon11@gmail.com>
2025-04-04 10:32:58 +0200

    smile

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -2,6 +2,7 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    # change the name of the backup if ur home manager fails again
     backupFileExtension = "backup";
     extraSpecialArgs = { inherit inputs; };
   };
