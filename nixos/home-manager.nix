28b00e64188fa09a52e0dcc6784f80b1bea9f97b
imKiyo <xargon11@gmail.com>
2025-07-02 21:49:16 +0200

    surely ill fix the backup bug some day

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -3,7 +3,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     # change the name of the backup if ur home manager fails again
-    backupFileExtension = "backup7";
+    backupFileExtension = "backup8";
     extraSpecialArgs = { inherit inputs; };
   };
 }
