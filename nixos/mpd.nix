fc0d6647e389ca13ae79e7d4867e2f52598baecd
imKiyo <xargon11@gmail.com>
2025-08-12 01:57:08 +0200

    sdsd

--- nixos/mpd.nix
+++ nixos/mpd.nix
@@ -4,12 +4,17 @@
   services.mpd = {
     enable = true;
     musicDirectory = "/home/kiyo/Music";
-    user = "kiyo";
+    user = "mpd";
+    group = "users";
     extraConfig = ''
       audio_output {
           type "pipewire"
           name "My PipeWire Output"
       }
     '';
+  };
+  
+  systemd.services.mpd.serviceConfig = {
+    ReadWritePaths = "/home/kiyo/Music";
   };
 }
