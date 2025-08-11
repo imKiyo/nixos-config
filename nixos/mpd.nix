dc0a34dd3ce5e34369da4f4bb6990e3578a8021d
imKiyo <xargon11@gmail.com>
2025-08-12 01:48:24 +0200

    sdasdas

--- nixos/mpd.nix
+++ nixos/mpd.nix
@@ -6,6 +6,7 @@
     musicDirectory = "/home/kiyo/Music";
     user = "mpd";
     group = "users";
+    extraAllowedDirs = [ "/home/kiyo" ];
     extraConfig = ''
       audio_output {
           type "pipewire"
