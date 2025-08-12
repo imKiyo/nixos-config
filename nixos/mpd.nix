fadeb1000c73b75bfee7c76cb6716dbdeae85334
imKiyo <xargon11@gmail.com>
2025-08-12 02:50:39 +0200

    sadas

--- nixos/mpd.nix
+++ nixos/mpd.nix
@@ -9,9 +9,21 @@
     group = "users";
     extraConfig = ''
       audio_output {
-          type "pulse"
-          name "PulseAudio Output"
+          type "alsa"
+          name "ALSA Output"
+          device "hw:CARD=USB,DEV=0"  # Your Scarlett interface
+      }
+      
+      # Fallback output
+      audio_output {
+          type "alsa"
+          name "HDMI Output"
+          device "hw:CARD=NVidia,DEV=3"  # Your HDMI output
+          enabled "no"
       }
     '';
   };
+  
+  # Ensure proper permissions
+  users.users.kiyo.extraGroups = [ "audio" ];
 }
