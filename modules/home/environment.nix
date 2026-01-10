4898a34dd9a72a88039178a0e1540eca29a4b508
kiyo <kiyo@nixos-desktop>
2026-01-10 22:37:19 +0100

    fsdfds

--- modules/home/environment.nix
+++ modules/home/environment.nix
@@ -1,8 +1,9 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.sessionVariables = {
     # CHROME_EXECUTABLE used by Flutter development
     CHROME_EXECUTABLE = "/run/current-system/sw/bin/google-chrome-stable";
     # BROWSER used by CLI tools and applications to open URLs
-    BROWSER = "xdg-open";
+    # BROWSER = "xdg-open";
   };
 }
