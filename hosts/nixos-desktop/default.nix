2139280ea95083b59c560d923f7765e32d57f656
kiyo <kiyo@nixos-desktop>
2026-01-10 06:03:46 +0100

    d

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -16,4 +16,23 @@
 
   # Ensure niri session is available to display manager
   services.displayManager.sessionPackages = [ pkgs.niri ];
+
+  services.ollama = {
+      enable = true;
+      package = pkgs.ollama-cuda.override {
+        cudaArches = [ "52" ];
+      };
+      loadModels = [ "dolphin-llama3:8b" ];
+    };
+
+    environmentVariables = {
+      # Forces Ollama to recognize Maxwell card's compute capability
+      OLLAMA_CUDA_COMPUTE_CAPABILITIES = "5.2";
+      # Helps with memory management on older 4GB cards
+      OLLAMA_NOPRUNE = "1";
+    };
+  };
+
+  # Firewall for local usage of ai if needed
+  # networking.firewall.allowedTCPPorts = [ 11434 ];
 }
