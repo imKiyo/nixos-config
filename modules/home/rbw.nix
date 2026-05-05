1f20afa1038c65315f2508d2727d214ecae248dc
kiyo <kiyo@nixos-desktop>
2026-05-05 22:40:04 +0200

    ghahahahahha

--- modules/home/rbw.nix
+++ modules/home/rbw.nix
@@ -2,7 +2,7 @@
   programs.rbw = {
     enable = true;
     settings = {
-      email = "xargon11@email.com";
+      email = "xargon11@gmail.com";
       base_url = "https://localhost";
       pinentry = pkgs.pinentry-gnome3;
     };
